dcea0e6c59ce43003c3de40f2c7ca32e1835fab5
Olivera Micic <omicic@gmail.com>
2021-04-23 11:25:17 +0200

    listener

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
  $.getJSON("./data/data.json", function (json) {
      console.log(json);
      document.body.innerHTML = display(JSON.parse(JSON.stringify(json)), "main");
+     setClicks();
  });
 
 
@@ -11,11 +12,40 @@
      text += `<ul class="${className}">`
      data.forEach(mainList => {
          (mainList.children.length > 0) ? text += `<li class="arrow">${mainList.name}`: text += `<li class="not_active">${mainList.name}</li>`;
-        if(mainList.children.length > 0) display(mainList.children[0], "nested")
-        text += `</li>`;
-        })
+         if (mainList.children.length > 0) display(mainList.children[0], "nested")
+         text += `</li>`;
+     })
 
-        text += `</ul>`;
+     text += `</ul>`;
 
-        return text;
+     return text;
+ }
+
+ function setClicks() {
+     let arrows = document.querySelectorAll('.arrow');
+     for (var i = 0; i < arrows.length; i++) {
+         arrows[i].addEventListener('click', function (event) {
+             event.stopPropagation();
+             if (!event.target.classList.contains('not-active')) {
+                 //da li je otvorena
+                 if (event.target.classList.contains('arrow-down')) {
+                     //otvorena
+                     let allNested = event.target.querySelectorAll('.nested');
+                     for (var i = 0; i < allNested.length; i++) {
+                         const nested = allNested[i];
+
+                         nested.classList.remove('active');
+                         nested.classList.remove('arrow-down');
+                         nested.parentElement.classList.remove('active');
+                         nested.parentElement.classList.remove('arrow-down');
+                     }
+                 } else {
+                     //zatvorena
+                     this.querySelector(".nested").classList.toggle('active');
+                     this.classList.toggle('arrow-down');
+                 }
+             }
+
+         })
+     }
  }
